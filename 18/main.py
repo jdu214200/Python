aa47efd593c4177d5d9ed9521680241b747a6fb1
Islom <123618830+jdu214200@users.noreply.github.com>
2023-10-31 23:24:36 +0500

    Add files via upload

---
+++ 18/main.py
@@ -0,0 +1,13 @@
+number = 5 # int
+
+digit = -4.54353432 # float
+word = "REzultat:" # string
+boolean = True # bool
+
+str_num = '5' # string
+
+# print(word + str(digit))
+
+print(word + str(number + bool(str_num)))
+
+del number
