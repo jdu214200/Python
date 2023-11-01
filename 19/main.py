9edf9289c2775e1801cb9af8165dc5df583fa27b
Islom <123618830+jdu214200@users.noreply.github.com>
2023-11-01 22:42:15 +0500

    Add files via upload

---
+++ 19/main.py
@@ -0,0 +1,17 @@
+num1 = input('Vedite pervoe chislo')
+
+num2 = input('Vedite vtoroy chislo')
+
+num1 -= 5
+
+print('Result:', num1 + num2)
+print('Result:', num1 - num2)
+print('Result:', num1 / num2)
+print('Result:', num1 * num2)
+print('Result:', num1 ** num2)
+print('Result:', num1 // num2)
+
+word = "HI"
+print(word * 2)
+
+word = 45
