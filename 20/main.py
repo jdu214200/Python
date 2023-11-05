3b3d89b6293a30876ded9de42e9c53474b2aa5a1
Islom <123618830+jdu214200@users.noreply.github.com>
2023-11-05 22:04:32 +0500

    Add files via upload

---
+++ 20/main.py
@@ -0,0 +1,15 @@
+user_data = int(input("vedite chislo"))
+
+isHappy = False
+
+if not isHappy:
+    print("User is happy")
+elif user_data == 5:
+    print("Number is 5")
+else:
+    print("User is unhappy")
+
+# if (user_data != 5):
+#     print("Mi na meste")
+#     if user_data > 6:
+#         print("Number is bigger than 5")
