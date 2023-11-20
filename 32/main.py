9543629cf1819617d2f80cc6ab4db14fa909d1f3
Islom <123618830+jdu214200@users.noreply.github.com>
2023-11-20 14:26:33 +0500

    Add files via upload

---
+++ 32/main.py
@@ -0,0 +1,12 @@
+data = (5, 7, True, 5.6, 'Hello')
+
+for el in data:
+    print(el)
+
+# print(data)
+nums = [5, 7, 3]
+
+new_data = tuple(nums)
+word = tuple('Hello world')
+# print(new_data)
+print(word)
