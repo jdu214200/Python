c223f0f040301a52dd415a0da15a4004fab8b129
Islom <123618830+jdu214200@users.noreply.github.com>
2023-11-23 19:39:13 +0500

    Add files via upload

---
+++ 35/main.py
@@ -0,0 +1,13 @@
+person = {
+    'user_1': {
+        'first_name': 'Jone',
+        'last_name': 'Mariy',
+        'age': 45,
+        'address': ('Moskva', 'ul. Kakay-to', '45'),
+        'grades': {'math': 5, 'physics': 3}
+    },
+    'user_2': {
+
+    }
+}
+print(person['user_1']['address'][1])
