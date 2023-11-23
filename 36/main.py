9d0eb830c7bf5179ac27f793b26bceef2913c89f
Islom <123618830+jdu214200@users.noreply.github.com>
2023-11-24 00:01:14 +0500

    Add files via upload

---
+++ 36/main.py
@@ -0,0 +1,15 @@
+# data = set('Hello')
+data = {5, 7, 4, 3, 5}
+
+data.add(32)
+data.update(['32', True, 4, 5])
+data.remove(True)
+data.pop()
+# data.clear()
+
+nums = [5, 7, 3, 5, 5]
+new_nums = set(nums)
+
+new_data = frozenset([5, 7, 4, '32', True, 4, 6, 5, 7, 4, 3, 5])
+# print(data)
+print(new_data)
