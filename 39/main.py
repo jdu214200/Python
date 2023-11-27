81e69d7514bb89dde296c455e71d95f60ae586e5
Islom <123618830+jdu214200@users.noreply.github.com>
2023-11-28 01:47:23 +0500

    Add files via upload

---
+++ 39/main.py
@@ -0,0 +1,17 @@
+nums1 = [2, 4, 6, 8]
+
+min = nums1[0]
+for el in nums1:
+    if el < min:
+        min = el
+
+print(min)
+
+nums2 = [2.4, 4.6, 6.8, 8.2]
+
+min2 = nums2[0]
+for el in nums2:
+    if el < min2:
+        min2 = el
+
+print(min2)
