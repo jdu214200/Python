c21522a8c54b2e096b32bfba34a3bf00f30b5d52
Islom <123618830+jdu214200@users.noreply.github.com>
2023-12-27 23:37:59 +0500

    Add files via upload

---
+++ 49/main.py
@@ -0,0 +1,18 @@
+class Cat:
+    name = None
+    age = None
+    isHappy =None
+
+
+cat1 = Cat()
+cat1.name = "Barsik"
+cat1.age = 3
+cat1.isHappy = True
+
+cat2 = Cat()
+cat2.name = "Jopen"
+cat2.age = 2
+cat2.isHappy = False
+
+print(cat1.name)
+print(cat2.name)
