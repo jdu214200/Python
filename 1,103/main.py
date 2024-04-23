ee3427de894902cc399fe4b8c1a4229beea610e1
Islom <123618830+jdu214200@users.noreply.github.com>
2024-04-23 13:22:05 +0500

    Add files via upload

---
+++ 1,103/main.py
@@ -0,0 +1,13 @@
+# Импорт библиотек
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report
+import tensorflow as tf
+from tensorflow.keras import layers, models
+from sklearn.datasets import load_digits
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import accuracy_score
